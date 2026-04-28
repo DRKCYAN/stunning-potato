8069fc0304fcb279b878e25cce89fd7819a49fa7
DRKCYAN <cyaneboiplayz@gmail.com>
2026-04-28 13:39:52 -0400

    Fix Vercel deployment config

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,6 +4,5 @@ import tailwind from '@astrojs/tailwind';
 export default defineConfig({
   integrations: [tailwind()],
   output: 'static',
-  site: 'https://drkcyan.github.io',
-  base: '/stunning-potato',
+  site: 'https://stunning-potato-31a21n0yv-cyaneboiplayz-9929s-projects.vercel.app',
 });
